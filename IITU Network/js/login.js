e2e4f0c125911704627b0710252d372b1b5f8961
Konopyanov Manap <68179994+konopyanovM@users.noreply.github.com>
2022-03-13 11:59:00 +0600

    Add files via upload

---
+++ IITU Network/js/login.js
@@ -0,0 +1,9 @@
+const $email = document.getElementById('email')
+const $password = document.getElementById('password')
+const $form = document.getElementById('login-form')
+const $error = document.getElementById('error')
+
+// $form.addEventListener('submit', (e) => {
+
+// })
+
